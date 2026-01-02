33dc7d958f340c0b96574f0a0aef1785f07bc4d5
Shin <dirianda187@gmail.com>
2026-01-02 16:13:01 +0700

    Add files via upload

---
+++ ven.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Perintah pertama
+sudo dnf install nodejs -y
+
+# Perintah kedua
+sudo dnf install wget -y
+
+# Perintah ketiga
+sudo dnf install unzip
+
+# Perintah pertama
+wget https://github.com/Dirianda12/BERKAS/releases/download/file2/ven-random.zip
+
+# Perintah kedua
+unzip ven-random
+
+# Perintah ketiga
+cd ven-random
+
+# Perintah pertama
+./v3.sh
